9e9101797ea1b24c67ec01e08f99d2430f94033e
Riary2024 <alejoyepes84@outlook.es>
2024-09-27 02:29:23 -0500

    Clase 3 Function Scope

---
+++ src/scope/function.js
@@ -0,0 +1,10 @@
+
+function greeting() {
+    let userName = 'Ana'; 
+    console.log(userName);  // Output -> Ana
+
+    if (userName === 'Ana') {
+        console.log(`Hello ${userName}!`);  // Output -> Hello Ana!
+    }  
+}
+greeting();
